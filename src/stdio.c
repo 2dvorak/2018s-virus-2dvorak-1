acc918c072e67fb2d87cd99e82b379e7c4c51462
Seungyeop Lee <2dvorak@kaist.ac.kr>
2018-04-30 06:03:24 +0900

    Add stdio.h and stdio.c
    For fread-like functions that uses system calls.

---
+++ src/stdio.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+int my_memcmp(const unsigned char *str1, const unsigned char *str2, size_t count)
+{
+  register const unsigned char *s1 = (const unsigned char*)str1;
+  register const unsigned char *s2 = (const unsigned char*)str2;
+
+  while (count-- > 0)
+    {
+      if (*s1++ != *s2++)
+	  return s1[-1] < s2[-1] ? -1 : 1;
+    }
+  return 0;
+}
+
