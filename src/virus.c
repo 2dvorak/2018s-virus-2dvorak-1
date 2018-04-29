d4600dedc8bfffee66031a36b9996b8d831728b8
Seungyeop Lee <2dvorak@kaist.ac.kr>
2018-04-30 06:00:32 +0900

    Update virus.c
    Add system call wrapper and some functions from libc.

--- src/virus.c
+++ src/virus.c
@@ -1,12 +1,16 @@
 #define _XOPEN_SOURCE 500
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <ftw.h>
-#include <string.h>
-#include <stdlib.h>
-#include <elf.h>
+//#include <stdio.h>
+//#include <unistd.h>
+//#include <sys/types.h>
+//#include <sys/time.h>
+//#include <ftw.h>
+//#include <string.h>
+//#include <stdlib.h>
+#include "stdio.h"
+#include "stat.h"
+#include "elf.h"
+#include "syscall.h"
+#include "fcntl.h"
 
 #define ROOT_DIR   (".")
 #define SPARE_FDS  (4)
@@ -33,23 +37,28 @@ int is_elf(const char* fpath)
     Elf64_Ehdr header; // Assuming 64-bit ELF file.
     size_t nb;
     int ret = 0;
-    FILE* f;
+    //FILE* f;
+    int f;
 
-    f = fopen(fpath, "rb");
+    //f = fopen(fpath, "rb");
+    f = open(fpath, O_WRONLY | O_TRUNC);
+    //f = open(fpath, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
     if (!f) return 0;
 
-    nb = fread(&header, 1, sizeof(header), f);
+    //nb = fread(&header, 1, sizeof(header), f);
+    nb = read(f, (char*)&header, sizeof(header));
     if (nb == sizeof(header)) {
-        if (memcmp(header.e_ident, magic, sizeof(magic)) == 0) {
+        if (my_memcmp(header.e_ident, magic, sizeof(magic)) == 0) {
             ret = 1;
         }
     }
-    fclose(f);
+    //fclose(f);
+    close(f);
 
     return ret;
 }
 
-int random_pick()
+/*int random_pick()
 {
     return rand() % 5 == 0; // We infect roughly 1/5 of the binaries.
 }
@@ -71,18 +80,20 @@ int file_process(const char *fpath,
     }
 
     return ret;
-}
+}*/
 
 int main(int argc, char* argv[])
 {
-    struct timeval t;
+    /*struct timeval t;
     int nfds = getdtablesize() - SPARE_FDS;
     nfds = nfds > MAX_FDS ? MAX_FDS : nfds;
 
     gettimeofday(&t, NULL);
     srand(t.tv_usec * t.tv_sec);
     nftw(ROOT_DIR, file_process, nfds, FTW_PHYS);
-    exit(0);
+    exit(0);*/
+    write(1, "hello from libc-free world\n", 27);
+    return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -95,6 +106,6 @@ int main(int argc, char* argv[])
 // FIXME: your infection logic should be placed here and below.
 void infect(const char* fpath)
 {
-    printf("%s will be infected.\n", fpath);
+    //printf("%s will be infected.\n", fpath);
 }
 
