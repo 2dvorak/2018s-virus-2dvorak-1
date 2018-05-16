a10f8e0ef182536cb02ca1324358b021dc1053cc
Seungyeop Lee <2dvorak@kaist.ac.kr>
2018-05-16 17:59:17 +0900

    Update

--- startDockerForTest.sh
+++ startDockerForTest.sh
@@ -1,5 +1,6 @@
 DOCKER_ID=$(docker run --rm -d -w /home/elfluenza -it debian)
-#docker cp ./test/ $DOCKER_ID:/home/elfluenza/test/
+docker cp ./src/ $DOCKER_ID:/home/elfluenza/src/
+docker cp ./Makefile $DOCKER_ID:/home/elfluenza/Makefile
 docker cp ./test_script.sh $DOCKER_ID:/home/elfluenza/test_script.sh
 docker attach $DOCKER_ID
 #docker rm -v $(docker ps -a -q -f status=exited)
